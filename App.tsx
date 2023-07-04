a45d0e7d6380012ddbda5347afc6b2f7fc064f3a
MrTuyennn <nguyenngoctuyen188@gmail.com>
2023-07-04 13:42:53 +0700

    init skia app RN

---
+++ App.tsx
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import type {PropsWithChildren} from 'react';
+import React from 'react';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {Canvas, Circle, Group} from '@shopify/react-native-skia';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const size = 256;
+  const r = size * 0.33;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+      }}>
+      <Canvas style={{flex: 1}}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={size - r} cy={r} r={r} color="magenta" />
+          <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
